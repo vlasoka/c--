b470f8529b566e6ea7b19981e63f5e3fc8c30a22
vlasoka <90462089+vlasoka@users.noreply.github.com>
2023-09-05 15:58:23 +0300

    Add files via upload

---
+++ lab2z3.cpp
@@ -0,0 +1,22 @@
+﻿// lab2z3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int m, q, s = 0;
+    std::cout << "enter your money ";
+    std::cin >> m;
+    q = m / 10;
+    s += q;
+    m = m - q * 10;
+    q = m / 5;
+    s += q;
+    m = m - q * 5;
+    q = m / 2;
+    s += q;
+    m = m - q * 2;
+    s += m;
+    std::cout << "quantity: " << s;
+}
