baba35998487624d8f140a6c2ea00c94b3de0b9e
vlasoka <90462089+vlasoka@users.noreply.github.com>
2023-09-16 16:59:06 +0300

    Add files via upload

---
+++ lab2z1.cpp
@@ -0,0 +1,43 @@
+﻿// lab2z1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int n;
+    bool t = true;
+    std::cout << "enter your number ";
+    std::cin >> n;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            t = false;
+            break;
+        }
+    }
+    if (t) {
+        int k = 1;
+        for (int i = 2; i < n; i++) {
+            bool f = true;
+            for (int j = 2; j <= sqrt(i); j++) {
+                if (i % j == 0) {
+                    f = false;
+                    break;
+                }
+            }
+            if (f) k += 1;
+        }
+        bool l = true;
+        if (k == 1) l = false;
+        for (int i = 2; i <= sqrt(k); i++) {
+            if (k % i == 0) {
+                l = false;
+                break;
+            }
+        }
+        if (l) std::cout << "it's super-prime";
+        else std::cout << "it's not super-prime";
+    }
+    else std::cout << "it's not super-prime";
+}
