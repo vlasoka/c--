e97ec94f728dfef981e4dc42fc1c1fda1b76b3a8
vlasoka <90462089+vlasoka@users.noreply.github.com>
2023-10-07 19:47:54 +0300

    Add files via upload

---
+++ lab3z2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double root(double a, double x) {
+    return (2 * x + a / (x * x)) / 3;
+}
+
+int main()
+{
+    double a;
+    cout << "enter number: ";
+    cin >> a;
+    cout << "root with pow: " << pow(a, 1.0 / 3) << endl;
+    double x = a;
+    for (int i = 0; i < 100; i++) {
+        x = root(a, x);
+    }
+    cout << "root with our function: " << x;
+}
