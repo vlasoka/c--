12666c64314c834a555de908011299c2e083fc36
vlasoka <90462089+vlasoka@users.noreply.github.com>
2023-10-07 18:42:28 +0300

    Add files via upload

---
+++ lab3z4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int summa(int n) {
+    if (n == 0) return 0;
+    return 5 * n + summa(n - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "enter n: ";
+    cin >> n;
+    cout << summa(n) << endl;
+}
