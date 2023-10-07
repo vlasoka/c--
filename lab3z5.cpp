5ff7230589e85fbcd285b719df18a2e729081933
vlasoka <90462089+vlasoka@users.noreply.github.com>
2023-10-07 20:15:12 +0300

    Add files via upload

---
+++ lab3z5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string bin(int n) {
+    if (n == 1) return "1";
+    if (n % 2 == 0) return "0" + bin(n / 2);
+    else return "1" + bin((n - 1) / 2);
+}
+
+int main()
+{
+    cout << "enter n: ";
+    int n;
+    cin >> n;
+    string s = bin(n);
+    reverse(s.begin(), s.end());
+    cout << s;
+}
