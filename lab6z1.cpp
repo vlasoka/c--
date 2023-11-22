23bf01e351bb93fe9ede199958a434cf0c4105fe
vlasoka <90462089+vlasoka@users.noreply.github.com>
2023-11-22 16:21:54 +0300

    Add files via upload

---
+++ lab6z1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ofstream out("output.txt", ios::out | ios::binary);
+    if (!out) {
+        cout << "fail openning the file\n";
+        return 1;
+    }
+    for (string s; getline(cin, s);) {
+        if (!s.empty()) {
+            out << s << '\n';
+        }
+    }
+    out.close();
+}
