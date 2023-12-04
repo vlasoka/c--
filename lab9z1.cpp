af97d74404a65f013f1d62ac840bddd02067e002
vlasoka <90462089+vlasoka@users.noreply.github.com>
2023-12-04 14:55:18 +0300

    Add files via upload

---
+++ lab9z1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class NotExistError {
+public:
+    NotExistError () : message ("triangle doesn't exist") {}
+    void printMessage() const { cout << message << endl; }
+private:
+    string message;
+};
+
+class Triangle {
+public:
+    void SetTriangle(double ka, double kb, double kc) {
+        a = ka;
+        b = kb;
+        c = kc;
+    }
+
+    double Ploschad() {
+        if ((a + b <= c) || (a + c <= b) || (b + c <= a)) {
+            throw NotExistError();
+        }
+        double p = (a + b + c) / 2;
+        double s = 0;
+        s = sqrt(p * (p - a) * (p - b) * (p - c));
+        return s;
+    }
+
+private:
+    double a = 0;
+    double b = 0;
+    double c = 0;
+};
+
+int main()
+{
+    Triangle t;
+    double a, b, c;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+    cout << "c = ";
+    cin >> c;
+    t.SetTriangle(a, b, c);
+    try {
+        double s = t.Ploschad();
+        cout << s << endl;
+    }
+    catch (NotExistError& error) {
+        cout << "Error: ";
+        error.printMessage();
+        return 1;
+    }
+}
