bac6aedb0c173e126d1d768b0fe12e384874db13
vlasoka <90462089+vlasoka@users.noreply.github.com>
2023-12-06 11:47:46 +0300

    Add files via upload

---
+++ lab14z2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<class vector>
+void Print(vector t, string sep) {
+	int s = t.size();
+	for (int i = 0; i < s - 1; i++) {
+		cout << t[i] << sep;
+	}
+	cout << t[s - 1] << "\n";
+}
+
+int main() {
+	vector<int> data = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	Print(data, ", "); 
+}
